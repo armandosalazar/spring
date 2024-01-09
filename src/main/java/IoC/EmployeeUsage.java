fb8d7249f184f4f2bc4a60010ab54c58e5491ee7
Armando Salazar <armandosalazar351@gmail.com>
2024-01-09 14:26:32 -0600

    feat: use beans for inversion of control

--- src/main/java/IoC/EmployeeUsage.java
+++ src/main/java/IoC/EmployeeUsage.java
@@ -1,9 +1,19 @@
 package IoC;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class EmployeeUsage {
 
     public static void main(String[] args) {
-        Employee employeeBoss = new EmployeeBoss();
-        System.out.println(employeeBoss.getTasks());
+//        Employee employeeBoss = new EmployeeBoss();
+//        System.out.println(employeeBoss.getTasks());
+
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("application-context.xml");
+
+        Employee employee = context.getBean("employee", Employee.class);
+        System.out.println(employee.getTasks());
+
+        context.close();
+
     }
 }
