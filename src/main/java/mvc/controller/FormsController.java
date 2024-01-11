d6bd6ef70f8238061b0d7728f87b55085d808a74
Armando Salazar <armandosalazar351@gmail.com>
2024-01-11 00:35:11 -0600

    feat: use @RequestParam over HttpServletRequest for get query params

--- src/main/java/mvc/controller/FormsController.java
+++ src/main/java/mvc/controller/FormsController.java
@@ -1,9 +1,10 @@
 package mvc.controller;
 
-import jakarta.servlet.http.HttpServletRequest;
+// import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class FormsController {
@@ -18,8 +19,9 @@ public class FormsController {
     }
 
     @RequestMapping("process-form-model")
-    public String processFormModel(HttpServletRequest request, Model model) {
-        String name = request.getParameter("name");
+    // public String processFormModel(HttpServletRequest request, Model model) {
+    public String processFormModel(@RequestParam("name") String name, Model model) {
+        // String name = request.getParameter("name");
         name = name.concat(" Welcome.");
         String message = "Message: ".concat(name);
 
