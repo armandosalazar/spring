a237f7cf35d956ed82a62bf77e12073287a49bb1
Armando Salazar <armandosalazar351@gmail.com>
2024-01-15 13:28:03 -0600

    feat: add delete in cascade

--- src/main/java/hibernate/OneToOneClient.java
+++ src/main/java/hibernate/OneToOneClient.java
@@ -1,7 +1,6 @@
 package hibernate;
 
 import hibernate.model.Client;
-import hibernate.model.ClientDetails;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
@@ -10,13 +9,19 @@ public class OneToOneClient {
         SessionFactory sessionFactory = HibernateUtils.getSessionFactory();
         Session session = sessionFactory.openSession();
 
-        Client client = new Client("John", "Smith", "New York");
-        ClientDetails clientDetails = new ClientDetails("www.johnsmith.com", "123456789", "no comments");
-        client.setClientDetails(clientDetails);
+        // Client client = new Client("John", "Smith", "New York");
+        // ClientDetails clientDetails = new ClientDetails("www.johnsmith.com", "123456789", "no comments");
+        // client.setClientDetails(clientDetails);
 
+        int id = 2;
         session.beginTransaction();
-        session.persist(client);
+        Client client = session.get(Client.class, id);
+        if (client != null) {
+            session.remove(client);
+        } else System.out.println("Client with id " + id + " not found");
+        // session.persist(client);
         session.getTransaction().commit();
+        System.out.println(client);
 
         session.close();
         sessionFactory.close();
